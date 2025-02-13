107de82ad1cd171a262de7a9380bd704cf2b7eea
ortonluke <ortonluke77@gmail.com>
2025-02-12 20:08:41 -0600

    bux fixes on the pi

--- Photo_Frame/downloader.py
+++ Photo_Frame/downloader.py
@@ -19,7 +19,7 @@ def authenticate_google_account():
 
     if not creds or not creds.valid:
         flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-        creds = flow.run_local_server(port=0)
+        creds = flow.run_local_server(port=8080)
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
