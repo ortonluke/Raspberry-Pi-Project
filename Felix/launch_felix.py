92ae34dc6b30ad8b91f7e3d6ac0ab2f0ef024ad2
ortonluke <ortonluke77@gmail.com>
2025-03-26 15:59:23 -0500

    crash fix

--- Felix/launch_felix.py
+++ Felix/launch_felix.py
@@ -4,4 +4,6 @@ import os
 # Ensure the environment variables are passed to the new terminal
 env = os.environ.copy()
 
-subprocess.Popen(["lxterminal", "-e", "bash", "-c", "python3 /home/pumpkin-pi/Projects/Felix/felix_chat.py"], env=env)
+# Modify the command to keep the terminal open even after execution
+command = "python3 /home/pumpkin-pi/Projects/Felix/felix_chat.py; read -p 'Press Enter to close...'"
+subprocess.Popen(["lxterminal", "-e", "bash", "-c", command], env=env)
